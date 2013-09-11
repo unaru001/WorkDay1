74665dfd036bd8b13ba36295a430f30c716242ef
idsc3101 <idsc3101@gmail.com>
2013-09-11 15:22:28 -0500

    Solved

--- problem1/BMICalc/BMICalc/Form1.Designer.vb
+++ problem1/BMICalc/BMICalc/Form1.Designer.vb
@@ -28,48 +28,53 @@ Partial Class Form1
         Me.txtHeight = New System.Windows.Forms.TextBox()
         Me.btnCalcBMI = New System.Windows.Forms.Button()
         Me.txtBMI = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtCalc = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'txtWeight
         '
-        Me.txtWeight.Location = New System.Drawing.Point(125, 34)
+        Me.txtWeight.Location = New System.Drawing.Point(83, 22)
+        Me.txtWeight.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.txtWeight.Name = "txtWeight"
-        Me.txtWeight.Size = New System.Drawing.Size(130, 26)
+        Me.txtWeight.Size = New System.Drawing.Size(88, 20)
         Me.txtWeight.TabIndex = 0
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(53, 37)
+        Me.Label1.Location = New System.Drawing.Point(35, 24)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(59, 20)
+        Me.Label1.Size = New System.Drawing.Size(41, 13)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Weight"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(53, 69)
+        Me.Label2.Location = New System.Drawing.Point(35, 45)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(56, 20)
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "Height"
         '
         'txtHeight
         '
-        Me.txtHeight.Location = New System.Drawing.Point(125, 66)
+        Me.txtHeight.Location = New System.Drawing.Point(83, 43)
+        Me.txtHeight.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.txtHeight.Name = "txtHeight"
-        Me.txtHeight.Size = New System.Drawing.Size(130, 26)
+        Me.txtHeight.Size = New System.Drawing.Size(88, 20)
         Me.txtHeight.TabIndex = 2
         '
         'btnCalcBMI
         '
-        Me.btnCalcBMI.Location = New System.Drawing.Point(125, 118)
+        Me.btnCalcBMI.Location = New System.Drawing.Point(83, 77)
+        Me.btnCalcBMI.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnCalcBMI.Name = "btnCalcBMI"
-        Me.btnCalcBMI.Size = New System.Drawing.Size(130, 33)
+        Me.btnCalcBMI.Size = New System.Drawing.Size(87, 21)
         Me.btnCalcBMI.TabIndex = 4
         Me.btnCalcBMI.Text = "Calculate BMI"
         Me.btnCalcBMI.UseVisualStyleBackColor = True
@@ -77,51 +82,56 @@ Partial Class Form1
         'txtBMI
         '
         Me.txtBMI.AutoSize = True
-        Me.txtBMI.Location = New System.Drawing.Point(74, 185)
+        Me.txtBMI.Location = New System.Drawing.Point(49, 120)
+        Me.txtBMI.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.txtBMI.Name = "txtBMI"
-        Me.txtBMI.Size = New System.Drawing.Size(38, 20)
+        Me.txtBMI.Size = New System.Drawing.Size(26, 13)
         Me.txtBMI.TabIndex = 6
         Me.txtBMI.Text = "BMI"
         '
-        'TextBox1
+        'txtCalc
         '
-        Me.TextBox1.Location = New System.Drawing.Point(125, 182)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(130, 26)
-        Me.TextBox1.TabIndex = 5
+        Me.txtCalc.Location = New System.Drawing.Point(83, 118)
+        Me.txtCalc.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtCalc.Name = "txtCalc"
+        Me.txtCalc.Size = New System.Drawing.Size(88, 20)
+        Me.txtCalc.TabIndex = 5
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(261, 37)
+        Me.Label3.Location = New System.Drawing.Point(174, 24)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(29, 20)
+        Me.Label3.Size = New System.Drawing.Size(20, 13)
         Me.Label3.TabIndex = 7
         Me.Label3.Text = "lbs"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(261, 69)
+        Me.Label4.Location = New System.Drawing.Point(174, 45)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(55, 20)
+        Me.Label4.Size = New System.Drawing.Size(38, 13)
         Me.Label4.TabIndex = 8
         Me.Label4.Text = "inches"
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(384, 260)
+        Me.ClientSize = New System.Drawing.Size(256, 169)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.txtBMI)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtCalc)
         Me.Controls.Add(Me.btnCalcBMI)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.txtHeight)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.txtWeight)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Form1"
         Me.Text = "BMI Calculator"
         Me.ResumeLayout(False)
@@ -134,7 +144,7 @@ Partial Class Form1
     Friend WithEvents txtHeight As System.Windows.Forms.TextBox
     Friend WithEvents btnCalcBMI As System.Windows.Forms.Button
     Friend WithEvents txtBMI As System.Windows.Forms.Label
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtCalc As System.Windows.Forms.TextBox
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents Label4 As System.Windows.Forms.Label
 
